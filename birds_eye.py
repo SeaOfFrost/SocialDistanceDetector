5b9b4f68a2ccb65983ffda912909e75a8a8f5f8d
balaji1312 <balaji1312@gmail.com>
2020-07-19 01:21:30 +0530

    Add files via upload

--- birds_eye.py
+++ birds_eye.py
@@ -23,20 +23,20 @@ def birdseye_plot(frm1)
     global height = frm1.shape[0]
     global width = frm1.shape[1]
 
-    cv2.namedWindow('frm1', frm1)
+	cv2.namedWindow('frm1', frm1)
     cv2.setMouseCallback('frm1',inp_pts)
 	while True:
-        cv2.imshow('frm1',frm1)
-        cv2.waitKey(1)
-        if len(mouse_pts) == 5:
-            cv2.destroyWindow("frm1")
-            break
+    	cv2.imshow('frm1',frm1)
+    	cv2.waitKey(1)
+    	if len(mouse_pts) == 5:
+    	    cv2.destroyWindow("frm1")
+    	    break
 	frame_map = birdseye_pers(frm1, box_pts)
-    birdseye_view = np.zeros((int(height * scale_h), int(width * scale_w), 3), np.uint8)
-    return birdseye_view
+
+    return frame_map
 
 #call function when plotting birdseye view for each frame
-def birdseye_nodes(frame, pedestrian_boxes, frame_map):
+def birdseye_nodes(midpoints, frame_map):
 
 	global birdseye_thresh = 120
 
@@ -50,11 +50,9 @@ def birdseye_nodes(frame, pedestrian_boxes, frame_map):
     blank_image[:] = solid_back_color
     warped_pts = []
 
-    for i in range(len(pedestrian_boxes)):
-        mid_point_x = int((pedestrian_boxes[i][1] * width + pedestrian_boxes[i][3] * width) / 2)
-        mid_point_y = int((pedestrian_boxes[i][0] * height + pedestrian_boxes[i][2] * height) / 2)
+    for i in range(midpoints):
 
-        pts = np.array([[[mid_point_x, mid_point_y]]], dtype="float32")
+        pts = np.array(midpoints, dtype="float32")
         warped_pt = cv2.perspectiveTransform(pts, frame_map)[0][0]
         warped_pt_scaled = [int(warped_pt[0] * scale_w), int(warped_pt[1] * scale_h)]
 
